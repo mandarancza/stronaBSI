ca19b6d10bc234a9b22024e1bd65355c8e5aa723
mandarancza <tenisremis@gmail.com>
2024-11-24 23:54:58 +0100

    sfd

--- glowna.php
+++ glowna.php
@@ -21,29 +21,27 @@ if (!isset($_SESSION['username'])) {
             <a href='logout.php'>Wyloguj</a>
         </div>
         <div class="links mainipageitems">
-            <a class="fill" href=paragraphs.html>Zad 2. Paragrafy</a>
+            <a class="fill" href=paragraphs.php>Zad 2. Paragrafy</a>
         </div>
         <div class="links mainipageitems">
-            <a class="fill" href=forms.html>Zad 2. Formularz</a>
+            <a class="fill" href=forms.php>Zad 2. Formularz</a>
         </div>
         <div class="links mainipageitems">
-            <a class="fill" href=paragraphs2.html>Zad 3. Paragrafy</a>
+            <a class="fill" href=paragraphs2.php>Zad 3. Paragrafy</a>
         </div>
         <div class="links mainipageitems">
-            <a class="fill" href=forms2.html>Zad 3. Formularz</a>
+            <a class="fill" href=forms2.php>Zad 3. Formularz</a>
         </div>
         <div class="links mainipageitems">
-            <a class="fill" href=calc.html>Zad 4. Kalkulator</a>
+            <a class="fill" href=calc.php>Zad 4. Kalkulator</a>
         </div>
         <div class="links mainipageitems">
-            <a class="fill" href=forms2regex.html>Zad 5. Formularz regex</a>
+            <a class="fill" href=forms2regex.php>Zad 5. Formularz regex</a>
         </div>
         <div class="links mainipageitems">
             <a class="fill" href=formsphp.php>Zad 6. Formularz PHP</a>
         </div>
-        <div class="links mainipageitems">
-            <a class="fill" href=login.php>Zad 6. Logowanie</a>
-        </div>
+        
     </div>
 </body>
 </html>
